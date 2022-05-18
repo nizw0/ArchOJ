d43881eead1ad052c776abd77d5bdfffca000341
nizw0 <nizw0p@gmail.com>
2022-05-18 19:12:25 +0800

    Chore: configure dependencies
    aws-amplify axios Bootstrap moment husky lint-staged

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createPinia } from 'pinia'
-
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
