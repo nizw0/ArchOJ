a0da0dff0252e1878ed76dbbdf9cc31b9499500a
nizw0 <nizw0p@gmail.com>
2022-05-18 20:13:09 +0800

    Feat: create Login page and routing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import { Amplify } from 'aws-amplify'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
+import awsConfig from './aws-exports'
 import router from './router'
+Amplify.configure(awsConfig)
 
 const app = createApp(App)
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <authenticator>
+      <template v-slot="{ user, signOut }">
+        <h1>username: {{ user.username }}</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </authenticator>
+  </div>
+</template>
+
+<script setup>
+import { Authenticator } from '@aws-amplify/ui-vue'
+import '@aws-amplify/ui-vue/styles.css'
+</script>
+
+<style lang="scss" scoped></style>
